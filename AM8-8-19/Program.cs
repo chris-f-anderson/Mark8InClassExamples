b7cfe1bd930e8f4faeaea5b501121e820f9608cc
Chris <cf_anderson@netzero.com>
2019-08-19 08:51:57 -0500

    How to do FizzBuzz recursively

---
+++ AM8-8-19/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AM8_8_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(1, 100);
+        }
+
+        static void FizzBuzz(int startNumber, int maxNumber)
+        {
+            if (startNumber % 3 == 0 && startNumber % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+            else if (startNumber % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (startNumber % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(startNumber);
+
+            if (startNumber == maxNumber)
+                return;
+
+            FizzBuzz(startNumber + 1, maxNumber);
+        }
+    }
+}
